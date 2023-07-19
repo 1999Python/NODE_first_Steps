5bbfae625477268def3dce27c7dee019dee00508
1999Python <raadiyaharend6@gmail.com>
2023-07-19 07:22:30 +0200

    1st commit

---
+++ greet.js
@@ -0,0 +1,18 @@
+function greet(Person){
+return "Hello, " + Person;
+}
+greet("Bob");
+greet("Sam");
+console.log(greet("Bob"));
+console.log(greet("Sam"));
+
+const msg = greet('Xola')
+console.log(msg)
+console.log(greet('Ntombi'));
+console.log(greet('Raadi'));
+
+console.log(greet('Saffah'));
+console.log(greet('Akhona'));
+console.log(greet('Hidayaat'));
+console.log(greet('Tendani'));
+console.log(greet('Ngomso'));
